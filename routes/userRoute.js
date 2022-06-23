b9b1a7b31025654f91ebae81cce6c677847598e7
Mateus Silva Souza <mateussilvasouza@pop-os.localdomain>
2022-06-23 18:59:43 -0300

    Mockando uma API

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,4 @@
 const fs = require('fs')
-
 const {join} = require('path')
 
 const filePath = join(__dirname, 'users.json')
@@ -22,9 +21,40 @@ const userRoute = (app) => {
     app.route('/users/:id?')
         .get((req,res)=>{
             const users = getUsers()
-
             res.send({ users })
+        })
+        .post((req, res)=>{
+            const users = getUsers()
+
+            users.push(req.body)
+            saveUser(users)
+
+            res.status(201).send('OK')
+        })
+        .put((req,res)=>{
+            const users = getUsers();
+
+            saveUser(users.map(user => {
+                if(user.id === req.params.id){
+                    return{
+                        ...user,
+                        ...req.body
+                    }
+                }
+
+                return user
+            }))
+
+            res.status(200).send('OK')
+        })
+
+        .delete((req,res)=>{
+            const users = getUsers()
+
+            saveUser(users.filter(user => user.id !== req.params.id))
+
+            res.status(200).send('OK')
         })
 }
 
-module.exports(userRoute)
+module.exports = userRoute
